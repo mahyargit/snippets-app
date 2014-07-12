a621e8b0ceccaff554f19518deaa0642ce08b8d6
Mahyar Rahmatian <imagemosic@gmail.com>
2014-07-11 18:40:37 -0700

    firstcommit

---
+++ snippets.py
@@ -0,0 +1,36 @@
+import logging
+import csv
+import argparse
+import sys
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="output.log", level=logging.DEBUG)
+
+def put(name, snippet, filename):
+    """ Store a snippet with an associated name in the CSV file """
+    logging.info("Writing {}:{} to {}".format(name, snippet, filename))
+    logging.debug("Opening file")
+    with open(filename, "a") as f:
+        writer = csv.writer(f)
+        logging.debug("Writing snippet to file".format(name, snippet))
+        writer.writerow([name, snippet])
+    logging.debug("Write sucessful")
+    return name, snippet
+
+def make_parser():
+    """ Construct the command line parser """
+    logging.info("Constructing parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+
+    return parser
+
+
+ def main():
+    """ Main function """
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
