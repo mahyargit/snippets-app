e8607c3ee02bc1ee99398dcc2c682dc63d9aa591
Mahyar Rahmatian <imagemosic@gmail.com>
2014-07-11 19:21:10 -0700

    2nd commit

--- snippets.py
+++ snippets.py
@@ -23,14 +23,32 @@ def make_parser():
     description = "Store and retrieve snippets of text"
     parser = argparse.ArgumentParser(description=description)
 
+    subparsers = parser.add_subparsers(help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    put_parser.add_argument("filename", default="snippets.csv", nargs="?",
+                            help="The snippet filename")
+    put_parser.set_defaults(command="put")
+
     return parser
 
 
- def main():
+def main():
     """ Main function """
     logging.info("Starting snippets")
     parser = make_parser()
     arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print "Stored '{}' as '{}'".format(snippet, name)
 
 if __name__ == "__main__":
     main()
